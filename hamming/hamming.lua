d29940589ad35c2663445cce6ec333db01c8af71
Leonardo Freua <leonardo.batista.freua@gmail.com>
2021-01-20 19:33:29 -0300

    Hamming: refactoring.

--- hamming/hamming.lua
+++ hamming/hamming.lua
@@ -1,15 +1,15 @@
 local Hamming = {}
 
 function Hamming.compute(a, b)
-  local h_distance = 0
-  if #a == #b then
-    for i = 1, #a do
-      if a:sub(i, i) ~= b:sub(i, i) then
-        h_distance = h_distance + 1
-      end
-    end
-  else
+  if #a ~= #b then
     return -1
+  end
+
+  local h_distance = 0
+  for i = 1, #a do
+    if a:sub(i, i) ~= b:sub(i, i) then
+      h_distance = h_distance + 1
+    end
   end
   return h_distance
 end
